0f7d808a4ffbe2b72496eaa912fa882bc4d1cf32
shrijithps <p.shrijith@talview.com>
2025-10-19 11:48:04 +0530

    removed_redundant_code

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,41 +1,47 @@
 import {type ReactElement} from 'react';
 
+const projectDetails = [
+    {
+        title: "YouTube Clone",
+        description: "A mini YouTube clone built with React, featuring trending videos, a functional search bar, sidebar navigation, and responsive video cards.",
+        image: "",
+        link: "https://github.com/psshrijith/youtubeCloneApp"
+    },
+    {
+        title: "Bankist App",
+        description: "A minimalistic banking application built using vanilla JavaScript to demonstrate key web development concepts like DOM manipulation, event handling, and working with arrays and timers.",
+        image: "",
+        link: "https://github.com/psshrijith/bankist_app"
+    },
+    {
+        title: "CineSearch",
+        description: "A sleek React app that uses the OMDb API to fetch and display detailed movie information. Users can search for any movie title and instantly view its poster, year, and type all in a clean, responsive layout.",
+        image: "",
+        link: "https://github.com/psshrijith/omdbapp"
+    },
+    {
+        title: "Portfolio",
+        description: "My web development journey - projects, experiments, and coding adventures.",
+        image: "",
+        link: "https://github.com/psshrijith/omdbapp"
+    }
+]
 const Projects=(): ReactElement => {
     return (
     <section id="projects" className="p-10 max-w-5xl mx-auto mb-10">
         <h3 className="text-3xl font-semibold mb-6 text-center text-gray-900">Projects</h3>
         <div className="flex justify-center gap-6">
-            <div className="w-80 p-6 bg-white/20 rounded-xl shadow-md hover:shadow-lg transition backdrop-blur-md">
-                <h4 className="font-bold text-lg mb-2 text-gray-900">YouTube Clone</h4>
-                <p className="text-sm mb-2 text-gray-900">
-                    A React + Tailwind clone of YouTube UI for practice.
-                </p>
-                <a href="https://github.com/psshrijith/youtubeCloneApp" className="text-blue-600 hover:underline">View Project</a>
-            </div>
-
-            <div className="w-80 p-6 bg-white/20 rounded-xl shadow-md hover:shadow-lg transition backdrop-blur-md">
-                <h4 className="font-bold text-lg mb-2 text-gray-900">Bankist App</h4>
-                <p className="text-sm mb-2 text-gray-900">
-                    A minimalistic banking application built using vanilla JavaScript to demonstrate key web development concepts like DOM manipulation, event handling, and working with arrays and timers.
-                </p>
-                <a href="https://github.com/psshrijith/bankist_app" className="text-blue-600 hover:underline">View Project</a>
-            </div>
-
-            <div className="w-80 p-6 bg-white/20 rounded-xl shadow-md hover:shadow-lg transition backdrop-blur-md">
-                <h4 className="font-bold text-lg mb-2 text-gray-900">CineSearch</h4>
-                <p className="text-sm mb-2 text-gray-900">
-                    A sleek React app that uses the OMDb API to fetch and display detailed movie information. Users can search for any movie title and instantly view its poster, year, and type — all in a clean, responsive layout.
-                </p>
-                <a href="https://github.com/psshrijith/omdbapp" className="text-blue-600 hover:underline">View Project</a>
-            </div>
-
-            <div className="w-80 p-6 bg-white/20 rounded-xl shadow-md hover:shadow-lg transition backdrop-blur-md">
-                <h4 className="font-bold text-lg mb-2 text-gray-900">Portfolio</h4>
-                <p className="text-sm mb-2 text-gray-900">
-                    My web development journey - projects, experiments, and coding adventures.
-                </p>
-                <a href="https://github.com/psshrijith/omdbapp" className="text-blue-600 hover:underline">View Project</a>
-            </div>
+            {projectDetails.map((project, index)=>{
+                return(
+                    <div key={index} className="w-80 p-6 bg-white/20 rounded-xl shadow-md hover:shadow-lg transition backdrop-blur-md">
+                        <h4 className="font-bold text-lg mb-2 text-gray-900">{project.title}</h4>
+                        <p className="text-sm mb-2 text-gray-900">
+                            {project.description}
+                        </p>
+                        <a href={project.link} className="text-blue-600 hover:underline">View Project</a>
+                    </div>
+                )
+            })}
         </div>
     </section>
     )
