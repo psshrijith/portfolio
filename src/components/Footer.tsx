7e0b3370c4d22961d79172ef222633ac1b9d724c
shrijithps <p.shrijith@talview.com>
2025-10-19 11:57:54 +0530

    fixed_footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import {type ReactElement} from "react";
 const Footer: () => ReactElement = () => {
     return (
         <footer className="text-center py-6 bg-white/50 text-gray-900 backdrop-blur-md">
-            © {new Date().getFullYear()} Shrijith. All rights reserved.
+            © {new Date().getFullYear()} Made by Shrijith.
         </footer>
     );
 };
