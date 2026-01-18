7d98331c9c77c247021373f97ac716f375f698ac
psshrijith <p.shrijith@talview.com>
2026-01-18 19:44:58 +0530

    fixed_projectdetails

--- src/components/Headers.tsx
+++ src/components/Headers.tsx
@@ -19,9 +19,19 @@ const Headers = (): ReactElement => {
         <header className="flex justify-between items-center p-6 bg-white/80 text-gray-900 sticky top-0 z-50 backdrop-blur-md">
             <h1 className="text-xl font-bold">Shrijith</h1>
             <nav className="space-x-4">
-                {links.map((link)=> (
-                    <a href={`#${(link?.link)?.toLowerCase()}`} className="hover:text-blue-600" key={link?.link}>{link?.link}</a>
-                ))}
+              {links.map((item) => (
+                <button
+                  key={item.link}
+                  onClick={() => {
+                    document
+                      .getElementById(item.link.toLowerCase())
+                      ?.scrollIntoView({ behavior: "smooth" });
+                  }}
+                  className="hover:text-blue-600"
+                >
+                  {item.link}
+                </button>
+              ))}
             </nav>
         </header>
     )
